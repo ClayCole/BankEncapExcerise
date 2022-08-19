fc7ce66faac3d2544d0d0c0e47d08eca7aa73222
Clay Coleman <claycoleman94@gamil.com>
2022-08-19 12:35:07 -0400

    inital upload

---
+++ BankEncapsulationExercise/BankAccount.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace BankEncapsulationExercise
+{
+    public class BankAccount
+    {
+        public BankAccount(double amount)
+        {
+            balance = amount;
+        }
+        
+        private double balance;
+
+        public void Deposit(double amount)
+        {
+            balance += amount;
+
+        }
+
+        public double GetBalance()
+        {
+            return balance;
+        }
+    }
+
+}
+
---
+++ BankEncapsulationExercise/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace BankEncapsulationExercise
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            
+
+            Console.WriteLine("Hello Bank of Cole User, Let's deposit some of your money to make me richer :)");
+            Console.WriteLine("How much are you depositing today? :");
+            var amount = double.Parse(Console.ReadLine());
+
+            BankAccount newAccount = new BankAccount(amount);
+
+            // Use switch statement to give user options of 
+
+            Console.WriteLine("Thank you user, Would you like to see your balance?");
+            var userResponse = Console.ReadLine().ToLower();
+
+            if (userResponse == "yes")
+            {                
+                Console.WriteLine($"${newAccount.GetBalance()}");
+            }
+            else
+            {
+                Console.WriteLine("Well to bad Im still showing you:");
+                Console.WriteLine($"${newAccount.GetBalance()}");
+            }
+
+
+
+
+        }
+    }
+}
+
